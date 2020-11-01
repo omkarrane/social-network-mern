294e04378344d2c2d414c88cae1fdb70045f6abc
Omkar Rane <omkarrane10@gmail.com>
2020-11-01 23:19:43 +0530

    added todo for date type

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const typeDefs = gql`
     id: ID!
     body: String!
     username: String!
-    createdAt: String!
+    createdAt: String!  # TODO: Change this to Date type
   }
 
   type Query {
