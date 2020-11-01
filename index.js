4ea95bef862872066017f52d6b1b2727815b606e
Omkar Rane <moneytornpa@moneytor.in>
2020-11-01 23:15:29 +0530

    Initial setup for Post and USer model

---
+++ index.js
@@ -0,0 +1,43 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { MONGO_URI } = require('./config');
+const Post = require('./models/Post');
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+
+  type Query {
+    getPosts: [Post]
+  }
+`
+
+const resolvers = {
+  Query: {
+    getPosts: async () => {
+      const posts = await Post.find();
+      return posts;
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("database connected successfully");
+    return server.listen({ port: 4000 });
+  })
+  .then(res => {
+    console.log(`server running at ${res.url}`);
+  });
